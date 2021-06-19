1b7bca6fa01901941a3c5008b5d689ddfda4d7f3
WIZARDOF-OZ <nurajabegum81@gmail.com>
2021-06-19 13:41:02 +0530

    invite

---
+++ Src/Commands/Owner/invite.js
@@ -0,0 +1,31 @@
+const Discord = require("discord.js");
+const db = require("old-wio.db");
+
+
+
+module.exports = {
+
+  name: "invite",
+  aliases: ["invitelink"],
+  category: "Owner",
+  description: "Give You My Invite Link",
+  usage: "invite",
+  ownerOnly: true,
+ 
+  execute: async (client, message, args) => {
+    
+    const Invite = `https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=bot`, Owne = `${Owner_Name}`, Dev = `Wizard`;
+    
+    const Embed = new Discord.MessageEmbed()
+    .setColor("RANDOM")
+    .setTitle("Thanks for using the bot")
+    .addField("Invite Me", `[Click Me](${Invite})`, true)
+    .addField("Support Server", `[Click Me](${support})`, true)
+    .addField("Owner", Owne, true)
+    .addField("Dev", Dev, true)
+    .setFooter(`Requested by ${message.author.username}`)
+    .setTimestamp();
+    
+    return message.channel.send(Embed).catch(() => message.channel.send("Invite Link - " + Invite));
+  }
+};
