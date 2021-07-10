4945bd4cf304d81e072e1854088eb0e4293c1a5f
WIZARDOF-OZ <nurajabegum81@gmail.com>
2021-07-10 11:37:16 +0530

    error cdoe 429

--- distube-handler.js
+++ distube-handler.js
@@ -12,7 +12,7 @@ module.exports = (client) => {
     leaveOnEmpty: true,
     leaveOnFinish: false,
     leaveOnStop: false,
-    // youtubeCookie --> prevents ERRORCODE: "429"
+    youtubeCookie: 'GPS=1; YSC=oAUkOcMWu4c; VISITOR_INFO1_LIVE=zr_RB6Gn-L4; PREF=tz=Asia.Calcutta; SID=_gc7sHXEDZOgHbGt2CSiZ9c-EMClYdNVjDKmPkwbVWakHmroasGwZGA8SxgyD_7GSSzMLg.; __Secure-3PSID=_gc7sHXEDZOgHbGt2CSiZ9c-EMClYdNVjDKmPkwbVWakHmroep7QDri33skMGIL5w32VvQ.; HSID=AWfnx0F9L8PAhc0v8; SSID=AmMGddsC_4AwVYhBC; APISID=ZrfF42dJBcTv842Z/A6Vb74vR3CAsiQETL; SAPISID=AurMx9BeIeKOlPBa/AS-JHLksnWLVZkWp7; __Secure-3PAPISID=AurMx9BeIeKOlPBa/AS-JHLksnWLVZkWp7; LOGIN_INFO=AFmmF2swRQIgPRTHoNKeMWYuFW5UJEhNgH3o6cSygj1DWPC05xoXufcCIQDxGnyBVvXicodsb8_wApz4kcnkknjKuDM35846PQwyBw:QUQ3MjNmemRzamtlNUYzN2NTUW5DRjZ1LUFnTXRJb1RQT3I2TEx0VWVvWmQ3WXN5NWlZNXNiYTVVOXRWRG5wVGc1bXpuMzZGVG5xS0QyaV9HakZRTW9CNlp4c2h5Y1l1anVzZGUxZmpYOHRoYnNsR2t0cGROd245ZUFtZURSTGJhd1YxemlOeGg2cGE2am5xU1IxU1R0SnYwZVctOXVGdXZ3; SIDCC=AJi4QfHT68gmzqwWDTjWJD5o0dxsoaPs2BWY0gXRsuOFioLjv_tLPS40FsXOyve-yAhY1wvZ; __Secure-3PSIDCC=AJi4QfEDfTM8s2xzDpS2ty0v4ROxuytL3WCgLTdo7wMr9dcZtGIDKp7BPhK1nt5qmOAwDZRK',
     youtubeDL: true,
     updateYouTubeDL: true,
     customFilters: {
