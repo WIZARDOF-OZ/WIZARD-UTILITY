acd645b3f7d24b2c411bc2eec2dcd0da2685f240
WIZARD OF OZ <nurajabegum81@gmail.com>
2021-08-02 22:52:54 +0530

    little update

--- Src/Commands/Fun/embed.js
+++ Src/Commands/Fun/embed.js
@@ -4,16 +4,23 @@ module.exports = {
     name:"embed",
     category:"Fun",
 
-    execute: (Client, message, args) => {
-        const embed = new Discord.MessageEmbed()
-
+    execute: async(Client, message, args, text, instance) => {
+        if(args.length === 0) { 
+        const errorembed = new Discord.MessageEmbed()
+        .setColor(instance.color.error)
+        .setDescription(`${instance.emoji.error}Please Provide Some Text`)
+        await message.inlineReply({embed: errorembed})
+        }
+const embed = new Discord.MessageEmbed()
         .setTitle(`Embed`)
         .setColor(`RANDOM`)
         .setThumbnail(message.guild.iconURL({dynamic:true}))
-        .setDescription(message.content.substring(7))
+        .setDescription(message.content.substring(6))
         .setFooter(`REQUESTED BY ${message.author.tag}`,`${message.author.displayAvatarURL({dynamic:true})}`)
         .setTimestamp();
-        message.channel.send(embed)
+       await message.inlineReply({
+           embed: embed
+       })
                 
     }
 }
